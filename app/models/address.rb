cdc95dc8c9e11c8fe5bb5c04413366521028ab4a
Washington Silva <w-osilva@hotmail.com>
2016-11-23 14:56:59 -0200

    Test for the address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,10 @@
 class Address < ApplicationRecord
   belongs_to :city
   belongs_to :state
+
+  validates :address, :presence => true
+  validates :county, :presence => true
+  validates :zip, :presence => true
+  validates :city, :presence => true
+  validates :state, :presence => true
 end
--- spec/factories/adresses.rb
+++ spec/factories/adresses.rb
@@ -1,9 +1,10 @@
 FactoryGirl.define do
   factory :address do
-    address "MyString"
-    county "MyString"
-    zip "MyString"
-    city nil
-    state nil
+    id { generate(:id) }
+    address Faker::Address.street_address
+    county Faker::Name.name
+    zip Faker::Address.zip
+    city { FactoryGirl.create(:city) }
+    state { FactoryGirl.create(:state) }
   end
 end
